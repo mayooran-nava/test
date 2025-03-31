a78f08d3b47a702aec5d986f7c6748f383d991b7
Mayooran Nava <134272538+mayooran-nava@users.noreply.github.com>
2025-03-31 16:58:20 +0100

    Update general.js

--- general.js
+++ general.js
@@ -16,7 +16,7 @@ const BASE_URL = 'https://qa-api.storyfeed.io/api/v1/embed'
 //open edit
 function Widget(element) {
   this.url = ''
-  this.baseURL = 'Krishna'
+  this.baseURL = 'Krishna2'
   this.perPage = null
   this.allFeeds = []
   this.widgetId = ''
